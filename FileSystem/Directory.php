064c2d1af398717aa2b01b675d90f62f0c0569ac
popnikos <popnikos@gmail.com>
2016-09-19 22:05:12 +0200

    Update Directory.php
    find method to checkout files from a folder using regular expression

--- FileSystem/Directory.php
+++ FileSystem/Directory.php
@@ -2,6 +2,29 @@
 
 namespace Popnikos\FileSystem;
 
+use DirectoryIterator;
+
 Trait Directory
 {
+    private $path;
+    
+    public function getPath() {
+        return $this->path;
+    }
+    
+    public function setPath( $path) {
+        $this->path = $path;
+    }
+    
+    public function find($path, $expression)
+    {
+        // Returned array
+        $found=[];
+        if (is_dir($path)) {
+            $iterator = new FilesystemIterator($this->path, 
+                FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_PATHNAME | FilesystemIterator::SKIP_DOTS);
+            $found = preg_grep($expression, iterator_to_array($iterator))    
+        }
+        return $found;
+    }
 }
